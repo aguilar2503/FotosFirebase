5287d35c30fd42c35e812cd8ac18595a52a6b36c
Roberto Aguilar <aguilar_.roberto@hotmail.com>
2019-11-26 20:07:02 -0700

    fotos firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { APP_ROUTES } from './app.routes';
+import { CargaImagenesService } from './services/carga-imagenes.service';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule, AngularFireStorage } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { NgDropFilesDirective } from './directives/ng-drop-files.directive';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FotosComponent,
+    CargaComponent,
+    NgDropFilesDirective,
+    PerfilComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule
+  ],
+  providers: [CargaImagenesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB8AsyW5q_jhs7y9PniibYu-pTd9LgCz6A',
+    authDomain: 'fotosfirebase-b8f61.firebaseapp.com',
+    databaseURL: 'https://fotosfirebase-b8f61.firebaseio.com',
+    projectId: 'fotosfirebase-b8f61',
+    storageBucket: 'fotosfirebase-b8f61.appspot.com',
+    messagingSenderId: '175793778014',
+    appId: '1:175793778014:web:fb719c605ea32ee36873cf'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
